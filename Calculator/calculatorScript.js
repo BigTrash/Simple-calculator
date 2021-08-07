40a7828ceeafb641452bea601e3eded41b41e44d
BigTrash <56404949+BigTrash@users.noreply.github.com>
2021-08-06 23:02:07 -0400

    efficiency changes, functions added for entering numbers and operators

--- Calculator/calculatorScript.js
+++ Calculator/calculatorScript.js
@@ -28,86 +28,45 @@ let value =""
 let nextOp = false;
 
 button1.addEventListener('click',function(){
-    //if an operation hasn't occured, then 1 is added to the value
-    //1 is displayed as apart of the current operand
-    if(!nextOp){
-        value += 1 
-        cOperand.innerHTML += 1
-        console.log(value)
-    }
+    //calling addNum function and passing a number parameter of 1
+    addNum(1)
 })
 
 button2.addEventListener('click',function(){
-    if(!nextOp){
-        value += 2 
-        cOperand.innerHTML += 2
-        console.log(value)
-    }
+    addNum(2)
     
 })
 
 button3.addEventListener('click',function(){
-    if(!nextOp){
-        value += 3
-        cOperand.innerHTML += 3
-        console.log(value)
-    }
+    addNum(3)
 })
 
 button4.addEventListener('click',function(){
-    if(!nextOp){
-        value += 4 
-        cOperand.innerHTML += 4
-        console.log(value)
-    }
+    addNum(4)
 })
 
 button5.addEventListener('click',function(){
-    if(!nextOp){
-        value += 5 
-        cOperand.innerHTML += 5 
-        console.log(value)
-    }
+    addNum(5)
 })
 
 button6.addEventListener('click',function(){
-    if(!nextOp){
-        value += 6 
-        cOperand.innerHTML += 6
-        console.log(value)
-    }
+    addNum(6)
 })
 
 button7.addEventListener('click',function(){
-    if(!nextOp){
-        value += 7 
-        cOperand.innerHTML += 7 
-        console.log(value)
-    }
+    addNum(7)
 })
 
 button8.addEventListener('click',function(){
-    if(!nextOp){
-        value += 8 
-        cOperand.innerHTML += 8 
-        console.log(value)
-    }
+    addNum(8)
 })
 
 button9.addEventListener('click',function(){
-    if(!nextOp){
-        value += 9 
-        cOperand.innerHTML += 9 
-        console.log(value)
-    }
+    addNum(9)
 })
 
 button0.addEventListener('click',function(){
-    if(!nextOp){
-        value += 0 
-        cOperand.innerHTML += 0 
-        console.log(value)
-    }
+    addNum(0)
 })
 
 clearButton.addEventListener('click',function(){
@@ -115,68 +74,30 @@ clearButton.addEventListener('click',function(){
     value = ""
     cOperand.innerHTML = ""
     pOperand.innerHTML = ""
+    console.log("cleared")
+    nextOp = false
 })
 
 addButton.addEventListener('click',function(){
-    //if an operation has occured
-    //this conditional exists to make the past operation text become current
-    //and current operation text to clearm while the total value stays the same
-    if(nextOp){
-        //past operation text becomes what is operation value(the answer)
-        pOperand.innerHTML = value
-        //current operation text clears
-        cOperand.innerHTML = "" 
-        //goes back to no operation, so the next operations can begin
-        nextOp = false
-        console.log("next operation: " + nextOp)
-    }
-
-    //operations contiue
-    value += "+" 
-    cOperand.innerHTML += "+" 
-    console.log(value)
+    //checking if an operation has occured by calling nextOperand function
+    //passing operand parameter of +
+    nextOperand("+")
 })
 
 subtractButton.addEventListener('click',function(){
 
-    if(nextOp){
-        pOperand.innerHTML = value
-        cOperand.innerHTML = "" 
-        nextOp = false
-        console.log("next operation: " + nextOp)
-    }
+    nextOperand("-")
 
-    value += "-" 
-    cOperand.innerHTML += "-" 
-    console.log(value)
 })
 
 multiplyButton.addEventListener('click',function(){
 
-    if(nextOp){
-        pOperand.innerHTML = value
-        cOperand.innerHTML = "" 
-        nextOp = false
-        console.log("next operation: " + nextOp)
-    }
-
-    value += "*" 
-    cOperand.innerHTML += "*" 
-    console.log(value)
+    nextOperand("*")
 })
 
 divideButton.addEventListener('click',function(){
 
-    if(nextOp){
-        pOperand.innerHTML = value
-        cOperand.innerHTML = "" 
-        nextOp = false
-        console.log("next operation: " + nextOp)
-    }
-
-    value += "/" 
-    cOperand.innerHTML += "÷"
-    console.log(value)
+    nextOperand("/")
 })
 
 deleteButton.addEventListener('click',function(){
@@ -188,11 +109,7 @@ deleteButton.addEventListener('click',function(){
 })
 
 decimalButton.addEventListener('click',function(){
-    if(!nextOp){
-        value += "." 
-        cOperand.innerHTML += "." 
-        console.log(value)
-    }
+    addNum(".")
 })
 
 equalsButton.addEventListener('click', function(){
@@ -207,3 +124,36 @@ equalsButton.addEventListener('click', function(){
     nextOp = true
     console.log("next operation: " + nextOp)
 })
+
+function addNum(numInput){
+    //if am operation hasn't occured
+    if(!nextOp){
+        //the number is added to the total value and the input text
+        value += numInput 
+        cOperand.innerHTML += numInput
+        console.log(value)
+    }
+}
+
+function nextOperand(operand){
+    //parameter is what operand is selected
+
+    //if an operation has occured
+    //this conditional exists to make the past operation text become current
+    //and current operation text to clear while the total value stays the same
+    if(nextOp){
+        //past operation text becomes what is operation value(the answer)
+        pOperand.innerHTML = value
+        //current operation text clears
+        cOperand.innerHTML = "" 
+        //goes back to no operation, so the next operations can begin
+        nextOp = false
+        console.log("next operation: " + nextOp)
+    }
+    
+    //operations contiue with the chosen operand
+    value += operand 
+    cOperand.innerHTML += operand
+    console.log(value)
+}
+
